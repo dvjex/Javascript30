d2114dfd9b2e411ef73056acfe93deb74432778d
DVJex <digmvijay@gmail.com>
2017-04-26 23:52:19 -0400

    Transitions

--- Drum Kit/drumkit.js
+++ Drum Kit/drumkit.js
@@ -1,5 +1,14 @@
+window.addEventListener('keydown', function(e){
+  var key = document.querySelector('.key[data-key="'+e.keyCode+'"]');
+  if(!key) {return;}
+  key.classList.add('playing');
+  setTimeout(function(){
+    key.classList.remove('playing');
+  },1);
+});
 window.addEventListener('keyup', function(e){
   var keyCode = e.keyCode;
+  var keys = document.getElementsByClassName('key');
   var audio = document.getElementsByTagName('audio');
 
   for(var i=0;i<audio.length;i++){
@@ -10,3 +19,8 @@ window.addEventListener('keyup', function(e){
     }
   }
 });
+
+keys = document.querySelector('.key');
+keys.for (var i = 0; i < keys.length; i++) {
+  keys[i].addEventListener('transitionend', removeTransition);
+}
