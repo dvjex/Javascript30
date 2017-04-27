4520afc068eb4f64f1bf16874d69f3ccc40a010b
DVJex <digmvijay@gmail.com>
2017-04-26 23:35:02 -0400

    Removed key selector

--- Drum Kit/drumkit.js
+++ Drum Kit/drumkit.js
@@ -1,10 +1,9 @@
 window.addEventListener('keyup', function(e){
   var keyCode = e.keyCode;
-  var keys = document.getElementsByClassName('key');
   var audio = document.getElementsByTagName('audio');
 
-  for(var i=0;i<keys.length;i++){
-    var keyValue = keys[i].getAttribute('data-key');
+  for(var i=0;i<audio.length;i++){
+    var keyValue = audio[i].getAttribute('data-key');
     if(keyCode == keyValue){
       audio[i].play();
       break;
