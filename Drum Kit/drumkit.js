018f22654b661cd89f6caa31e9ec710c7763ff71
DVJex <digmvijay@gmail.com>
2017-04-27 01:09:08 -0400

    Completed basic Drumkit.

--- Drum Kit/drumkit.js
+++ Drum Kit/drumkit.js
@@ -1,26 +1,28 @@
-window.addEventListener('keydown', function(e){
-  var key = document.querySelector('.key[data-key="'+e.keyCode+'"]');
+window.addEventListener('keydown', playSound);
+
+function playSound{
+  var keyCode = e.keyCode;
+  var key = document.querySelector('.key[data-key="'+keyCode+'"]');
   if(!key) {return;}
   key.classList.add('playing');
-  setTimeout(function(){
-    key.classList.remove('playing');
-  },1);
-});
-window.addEventListener('keyup', function(e){
-  var keyCode = e.keyCode;
-  var keys = document.getElementsByClassName('key');
-  var audio = document.getElementsByTagName('audio');
 
+  var audio = document.getElementsByTagName('audio');
   for(var i=0;i<audio.length;i++){
     var keyValue = audio[i].getAttribute('data-key');
     if(keyCode == keyValue){
+      audio[i].currentTime=0;
       audio[i].play();
       break;
     }
   }
 });
+function removeTransition(e)
+{
+  if(e.propertyName != 'transform') return;
+  this.classList.remove('playing');
+}
 
-keys = document.querySelector('.key');
-keys.for (var i = 0; i < keys.length; i++) {
+const keys = document.querySelectorAll('.key');
+for (var i = 0; i < keys.length; i++) {
   keys[i].addEventListener('transitionend', removeTransition);
 }
